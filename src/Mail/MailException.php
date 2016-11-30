db95a719f1112b344b8096149d4160a85301b200
Dmitry A. Nezhelskoy <dmitry@nezhelskoy.pro>
2016-11-30 14:30:52 +0300

    add new namespaces from master branch

---
+++ src/Mail/MailException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Onphp Extensions Package
+ * 
+ * @author Dmitry Nezhelskoy <dmitry@nezhelskoy.pro>
+ * @copyright 2014-2016 Barzmann Internet Solutions GmbH
+ */
+
+namespace Onphp\Extensions\Mail;
+
+/**
+ * Class MailException
+ */
+class MailException extends \Exception {
+	
+}
