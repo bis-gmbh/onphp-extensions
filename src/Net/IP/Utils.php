ac1b991aa8c3a222a8877c6be83c1b59d9e56a49
Dmitry A. Nezhelskoy <dmitry@nezhelskoy.pro>
2017-10-27 00:29:24 +0300

    bis-gmbh/2ip#146 - basic ip utils class

---
+++ src/Net/IP/Utils.php
@@ -0,0 +1,188 @@
+<?php
+/**
+ * Onphp Extensions Package
+ * 
+ * @author Dmitry A. Nezhelskoy <dmitry@nezhelskoy.pro>
+ * @copyright 2014-2017 Barzmann Internet Solutions GmbH
+ */
+
+namespace Onphp\Extensions\Net\IP;
+
+class Utils
+{
+    const REGEXP_IP = '/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){0,3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/i';
+    const REGEXP_CIDR = '/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){0,3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\/([0-9]|1[0-9]|2[0-9]|3[0-2])$/i';
+
+    public static $privateNetworks = ['10/8', '172.16/12', '192.168/16']; // rfc1918
+    public static $multicastNetworks = ['224/4']; // rfc3171
+    public static $reservedNetworks = ['240/4']; // rfc1112
+    public static $networkTypes = [ // rfc5735
+        [
+            'AddressBlock' => '0.0.0.0/8',
+            'PresentUse' => '"This" Network',
+            'Reference' => 'RFC 1122, Section 3.2.1.3',
+        ], [
+            'AddressBlock' =>'10.0.0.0/8',
+            'PresentUse' =>'Private-Use Networks',
+            'Reference' =>'RFC 1918'
+        ], [
+            'AddressBlock' =>'127.0.0.0/8',
+            'PresentUse' =>'Loopback',
+            'Reference' =>'RFC 1122, Section 3.2.1.3'
+        ], [
+            'AddressBlock' =>'169.254.0.0/16',
+            'PresentUse' =>'Link Local',
+            'Reference' =>'RFC 3927'
+        ], [
+            'AddressBlock' =>'172.16.0.0/12',
+            'PresentUse' =>'Private-Use Networks',
+            'Reference' =>'RFC 1918'
+        ], [
+            'AddressBlock' =>'192.0.0.0/24',
+            'PresentUse' =>'IETF Protocol Assignments',
+            'Reference' =>'RFC 5736'
+        ], [
+            'AddressBlock' =>'192.0.2.0/24',
+            'PresentUse' =>'TEST-NET-1',
+            'Reference' =>'RFC 5737'
+        ], [
+            'AddressBlock' =>'192.88.99.0/24',
+            'PresentUse' =>'6to4 Relay Anycast',
+            'Reference' =>'RFC 3068'
+        ], [
+            'AddressBlock' =>'192.168.0.0/16',
+            'PresentUse' =>'Private-Use Networks',
+            'Reference' =>'RFC 1918'
+        ], [
+            'AddressBlock' =>'198.18.0.0/15',
+            'PresentUse' =>'Network Interconnect, Device Benchmark Testing',
+            'Reference' =>'RFC 2544'
+        ], [
+            'AddressBlock' =>'198.51.100.0/24',
+            'PresentUse' =>'TEST-NET-2',
+            'Reference' =>'RFC 5737'
+        ], [
+            'AddressBlock' =>'203.0.113.0/24',
+            'PresentUse' =>'TEST-NET-3',
+            'Reference' =>'RFC 5737'
+        ], [
+            'AddressBlock' =>'224.0.0.0/4',
+            'PresentUse' =>'Multicast',
+            'Reference' =>'RFC 3171'
+        ], [
+            'AddressBlock' =>'240.0.0.0/4',
+            'PresentUse' =>'Reserved for Future Use',
+            'Reference' =>'RFC 1112, Section 4'
+        ], [
+            'AddressBlock' =>'255.255.255.255/32',
+            'PresentUse' =>'Limited Broadcast',
+            'Reference' =>'RFC 919, Section 7, RFC 922, Section 7'
+        ]
+    ];
+
+    protected static $octetCount = 4;
+    protected static $octetOffsets = [24, 16, 8, 0];
+    protected static $octetMasks = [0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF];
+
+    public static function isLong($long): bool
+    {
+        return is_int($long) && ($long >= 0 && $long <= 0xFFFFFFFF);
+    }
+
+    public static function isString($addr): bool
+    {
+        return is_string($addr) && (bool)preg_match(self::REGEXP_IP, $addr);
+    }
+
+    public static function isCIDR($cidr): bool
+    {
+        return is_string($cidr) && (bool)preg_match(self::REGEXP_CIDR, $cidr);
+    }
+
+    public static function detectFormat($addr): string
+    {
+        if (self::isLong($addr)) {
+            return 'numeric';
+        }
+        if (self::isString($addr)) {
+            return 'string';
+        }
+        if (self::isCIDR($addr)) {
+            return 'cidr';
+        }
+
+        return 'unknown';
+    }
+
+    public static function getLongPrefixFromCIDR($cidr): int
+    {
+        if (self::isCIDR($cidr) === false) {
+            throw new \InvalidArgumentException('Wrong cidr format');
+        }
+
+        $cidrParts = explode('/', $cidr);
+
+        return self::toLong($cidrParts[0]);
+    }
+
+    public static function getMaskBitsFromCIDR($cidr): int
+    {
+        if (self::isCIDR($cidr) === false) {
+            throw new \InvalidArgumentException('Wrong cidr format');
+        }
+
+        $mask = 0xFFFFFFFF;
+        $cidrParts = explode('/', $cidr);
+
+        for ($i=0; $i<$cidrParts[1]; $i++) {
+            $mask >>= 1;
+        }
+
+        return $mask;
+    }
+
+    public static function toLong(string $addr): int
+    {
+        if (self::isString($addr) === false) {
+            throw new \InvalidArgumentException('Wrong addr format');
+        }
+
+        $num = 0;
+        $octets = explode('.', $addr, self::$octetCount);
+        $restoredZeroOctetsCount = self::$octetCount - count($octets);
+
+        for ($i=0; $i<$restoredZeroOctetsCount; $i++) {
+            array_push($octets, "0");
+        }
+
+        for ($i=0; $i<self::$octetCount; $i++) {
+            $num |= intval($octets[$i]) << self::$octetOffsets[$i];
+        }
+
+        return $num;
+    }
+
+    public static function toString(int $addr): string
+    {
+        if (self::isLong($addr) === false) {
+            throw new \InvalidArgumentException('Wrong addr format');
+        }
+
+        $octets = [];
+
+        for ($i=0; $i<self::$octetCount; $i++) {
+            array_push($octets, ($addr & self::$octetMasks[$i]) >> self::$octetOffsets[$i]);
+        }
+
+        return implode('.', $octets);
+    }
+
+    public static function toBinaryString(int $nMask): string
+    {
+        if ($nMask < 0 || $nMask > 4294967295) {
+            throw new \OutOfRangeException('Argument $nMask must be between 0 and 4294967295');
+        }
+
+        return str_pad(decbin($nMask), 32, '0', STR_PAD_LEFT);
+    }
+}
---
+++ tests/Net/IPUtilsTest.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Onphp Extensions Package
+ * 
+ * @author Dmitry A. Nezhelskoy <dmitry@nezhelskoy.pro>
+ * @copyright 2014-2017 Barzmann Internet Solutions GmbH
+ */
+
+use \Onphp\Extensions\Net\IP\Utils as IPUtils;
+
+class IPUtilsTest extends PHPUnit_Framework_TestCase
+{
+    public function testIsLong()
+    {
+        $this->assertEquals(IPUtils::isLong(null), false);
+        $this->assertEquals(IPUtils::isLong(false), false);
+        $this->assertEquals(IPUtils::isLong(true), false);
+        $this->assertEquals(IPUtils::isLong('127.0.0.1'), false);
+        $this->assertEquals(IPUtils::isLong(-1), false);
+        $this->assertEquals(IPUtils::isLong(4294967296), false);
+        $this->assertEquals(IPUtils::isLong(0), true);
+        $this->assertEquals(IPUtils::isLong(2130706433), true);
+    }
+
+    public function testIsString()
+    {
+        $this->assertEquals(IPUtils::isString(null), false);
+        $this->assertEquals(IPUtils::isString(''), false);
+        $this->assertEquals(IPUtils::isString('300'), false);
+        $this->assertEquals(IPUtils::isString('127.0.256.1'), false);
+        $this->assertEquals(IPUtils::isString('0.0.0.0'), true);
+        $this->assertEquals(IPUtils::isString('127.0.0.1'), true);
+        $this->assertEquals(IPUtils::isString('10.0.100.1'), true);
+        $this->assertEquals(IPUtils::isString('224.1.0.0'), true);
+        $this->assertEquals(IPUtils::isString('255.255.255.255'), true);
+        $this->assertEquals(IPUtils::isString('169.255.255'), true);
+        $this->assertEquals(IPUtils::isString('169.255'), true);
+        $this->assertEquals(IPUtils::isString('169'), true);
+    }
+
+    public function testIsCIDR()
+    {
+        $this->assertEquals(IPUtils::isCIDR(null), false);
+        $this->assertEquals(IPUtils::isCIDR(''), false);
+        $this->assertEquals(IPUtils::isCIDR('/'), false);
+        $this->assertEquals(IPUtils::isCIDR('127.0.0.1'), false);
+        $this->assertEquals(IPUtils::isCIDR('0.0.400.0/0'), false);
+        $this->assertEquals(IPUtils::isCIDR('127.0.0.1/33'), false);
+        $this->assertEquals(IPUtils::isCIDR('192.168.100.2/'), false);
+        $this->assertEquals(IPUtils::isCIDR('/30'), false);
+        $this->assertEquals(IPUtils::isCIDR('0.0.0.0/0'), true);
+        $this->assertEquals(IPUtils::isCIDR('192.168.100.2/30'), true);
+        $this->assertEquals(IPUtils::isCIDR('192.168.100/24'), true);
+        $this->assertEquals(IPUtils::isCIDR('192.168/16'), true);
+        $this->assertEquals(IPUtils::isCIDR('10/8'), true);
+        $this->assertEquals(IPUtils::isCIDR('255.255.255.255/32'), true);
+    }
+
+    public function testDetectFormat()
+    {
+        $this->assertEquals(IPUtils::detectFormat(null), 'unknown');
+        $this->assertEquals(IPUtils::detectFormat(true), 'unknown');
+        $this->assertEquals(IPUtils::detectFormat(false), 'unknown');
+        $this->assertEquals(IPUtils::detectFormat(new \StdClass), 'unknown');
+        $this->assertEquals(IPUtils::detectFormat(''), 'unknown');
+        $this->assertEquals(IPUtils::detectFormat(2130706433), 'numeric');
+        $this->assertEquals(IPUtils::detectFormat('127.0.0.1'), 'string');
+        $this->assertEquals(IPUtils::detectFormat('192.168.100.2/30'), 'cidr');
+    }
+
+    public function testGetLongPrefixFromCIDR()
+    {
+        $this->assertEquals(IPUtils::getLongPrefixFromCIDR('127.0.0.1/8'), 2130706433);
+
+        $this->expectException('InvalidArgumentException');
+        $this->assertEquals(IPUtils::getLongPrefixFromCIDR('127.0.0.1'), 2130706433);
+    }
+
+    public function testGetMaskBitsFromCIDR()
+    {
+        $this->assertEquals(IPUtils::getMaskBitsFromCIDR('127.0.0.1/8'), 16777215);
+
+        $this->expectException('InvalidArgumentException');
+        $this->assertEquals(IPUtils::getMaskBitsFromCIDR('127.0.0.1'), 8);
+    }
+
+    public function testToLong()
+    {
+        $this->assertEquals(IPUtils::toLong('127.0.0.1'), 2130706433);
+
+        $this->expectException('InvalidArgumentException');
+        $this->assertEquals(IPUtils::toLong(''), 0);
+        $this->assertEquals(IPUtils::toLong('127.0.0.1/8'), 2130706433);
+    }
+
+    public function testToString()
+    {
+        $this->assertEquals(IPUtils::toString(3325256815), '198.51.100.111');
+        $this->assertEquals(IPUtils::toString(0), '0.0.0.0');
+        $this->assertEquals(IPUtils::toString(4294967295), '255.255.255.255');
+
+        $this->expectException('InvalidArgumentException');
+        $this->assertEquals(IPUtils::toString(4294967296), '255.255.255.255');
+        $this->assertEquals(IPUtils::toString(3325256815), '198.51.100.111/8');
+    }
+
+    public function testToBinaryString()
+    {
+        $this->assertEquals(IPUtils::toBinaryString(0), "00000000000000000000000000000000");
+        $this->assertEquals(IPUtils::toBinaryString(3325256815), '11000110001100110110010001101111');
+        $this->assertNotEquals(IPUtils::toBinaryString(3325256815), '01000110001100110110010001101111');
+        $this->assertEquals(IPUtils::toBinaryString(4294967295), '11111111111111111111111111111111');
+        $this->assertNotEquals(IPUtils::toBinaryString(4294967294), '11111111111111111111111111111111');
+
+        $this->expectException('OutOfRangeException');
+        $this->assertEquals(IPUtils::toBinaryString(-1), '');
+        $this->assertEquals(IPUtils::toBinaryString(4294967296), '');
+    }
+}
